355094bf71602fa9f1554c89c370d4ae759bedb6
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-04 11:32:35 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -25,6 +25,10 @@ def start_pannel(_):
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
+               InlineKeyboardButton(
+                text=_["S_B_9"],
+                 url=f"https://t.me/+HV1MWrssifhkOTk1"
+            ),
         ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
